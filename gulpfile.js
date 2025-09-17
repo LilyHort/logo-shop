8bd3b15676c0bbba4824d8676dac1acd5dd4855a
Лилиана Леднева <lilyledneva@gmail.com>
2025-09-17 12:16:51 +0300

    Добавляет стили order

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,10 @@ let isDevelopment = true;
 
 export function processMarkup () {
   return src(`${PATH_TO_SOURCE}**/*.html`)
-    .pipe(htmlmin({ collapseWhitespace: !isDevelopment }))
+    .pipe(htmlmin({
+      collapseWhitespace: !isDevelopment,
+      ignoreCustomFragments: [/\s*srcset\s*=\s*"[^"]*"/gi]
+    }))
     .pipe(dest(PATH_TO_DIST))
     .pipe(server.stream());
 }
